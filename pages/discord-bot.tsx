cfeaeb9828b5b7a621f13284b32c00e130247dd6
samira abdi <93336502+Samira-ABDI79@users.noreply.github.com>
2023-07-03 15:52:42 +0330

    feat (pages) : add Partners and discordBot

---
+++ pages/discord-bot.tsx
@@ -0,0 +1,38 @@
+
+export default function discordBot(){
+    return(
+        <>
+        <main className="flex justify-between lg:flex-row flex-col">
+<div className=" w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
+<div className="px-4  sm:px-8 xl:pr-16">
+<h1 className="text-4xl leading-snug tracking-tight font-extrabold text-gray-900 dark:text-gray-300 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+<span className="block xl:inline">Add value to your Community with</span>
+<span className="block text-indigo-600 dark:text-lime-500 xl:inline">our Discord Bot</span>
+</h1>
+<p className="mt-3  mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 w-full l">
+NFT Calendar is the most popular community-driven data source of upcoming NFT drops. You can add our bot to your server to notify your subscribers about the most promising collection launches before they happen.
+</p>
+<div className="mt-10 sm:flex sm:justify-center lg:justify-start">
+<div className="rounded-md shadow">
+<a href="https://forms.gle/jEC7c4Hgi5zxnoVn7" rel="nofollow" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 dark:text-black dark:bg-gradient-to-r dark:from-lime-500 dark:bg-amber-300  dark:hover:bg-lime-400 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+Contact Us
+</a>
+</div>
+<div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+<a href="https://discord.gg/rsPxj8TTft" rel="nofollow" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white dark:border-2 dark:border-lime-500 dark:bg-zinc-800 dark:text-lime-500 dark:hover:bg-lime-400 dark:hover:text-black hover:bg-gray-50 md:py-4 md:text-lg md:px-10">
+See in Action
+</a>
+</div>
+</div>
+</div>
+</div>
+<div className="flex justify-center items-center  w-full    py-[2rem]  ">
+
+<img className="  w-full inset-0 object-cover" 
+src="/assets/images/nftcalendarbot.webp" 
+alt=""/>
+</div>
+</main>
+        </>
+    )
+}
---
+++ pages/partners.tsx
@@ -0,0 +1,83 @@
+export default function  Partners(){
+    return(
+        <>
+        <section>
+<div className="lg:mt-6 mx-auto container">
+<div className="rounded p-6 lg:py-12 md:mx-10 md:p-10 my-8 bg-white dark:bg-zinc-800 dark:border-gray-700">
+<h1 className="text-2xl lg:text-3xl font-serif dark:text-yellow-100">We’re Proud to be Partners with</h1>
+<div className="px-2 py-10 flex flex-wrap">
+
+<div className="max-w-sm bg-white dark:bg-zinc-800 mb-6 md:w-1/4">
+<a href="https://bitcourier.co.uk" className="" target="_blank">
+<div className="rounded shadow-lg overflow-hidden mx-2 border dark:border-gray-700">
+<img className="w-full" src="/assets/images/bitcourier_.webp" alt=""/>
+<div className="px-6 py-2 content text-center">
+<div className="font-bold text-lg mb-1">
+bitcourier.co.uk
+</div>
+<div className="text-gray-700 dark:text-gray-300 text-lg">
+UK Cryptocurrency Media Outlet
+</div>
+</div>
+</div>
+</a>
+</div>
+
+<div className="max-w-sm bg-white dark:bg-zinc-800 mb-6 md:w-1/4">
+<a href="https://nftndx.io" className="" target="_blank">
+<div className="rounded shadow-lg overflow-hidden mx-2 border dark:border-gray-700">
+<img className="w-full" src="/assets/images/nftndx_logo.webp" alt=""/>
+<div className="px-6 py-2 content text-center">
+<div className="font-bold text-lg mb-1">
+nftndx.io
+</div>
+<div className="text-gray-700 dark:text-gray-300 text-lg">
+A trusted NFT explorer project
+</div>
+</div>
+</div>
+</a>
+</div>
+
+<div className="max-w-sm bg-white dark:bg-zinc-800 mb-6 md:w-1/4">
+<a href="https://www.brydge.network/" className="" target="_blank">
+<div className="rounded shadow-lg overflow-hidden mx-2 border dark:border-gray-700">
+<img className="w-full" src="/assets/images/brydge_logo.webp" alt=""/>
+<div className="px-6 py-2 content text-center">
+<div className="font-bold text-lg mb-1">
+brydge.network
+</div>
+<div className="text-gray-700 dark:text-gray-300 text-lg">
+Mint NFTs with any token on any chain
+</div>
+</div>
+</div>
+</a>
+</div>
+
+<div className="max-w-sm bg-white dark:bg-zinc-800 mb-6 md:w-1/4">
+<a href="https://niftykit.com/?ref=nftcalendar" className="" target="_blank">
+<div className="rounded shadow-lg overflow-hidden mx-2 border dark:border-gray-700">
+<img className="w-full" src="/assets/images/niftykiy.png" alt=""/>
+<div className="px-6 py-2 content text-center">
+<div className="font-bold text-lg mb-1">
+NiftyKit
+</div>
+<div className="text-gray-700 dark:text-gray-300 text-lg">
+Mint, manage, and sell NFT assets without help from developers.
+</div>
+</div>
+</div>
+</a>
+</div>
+</div>
+<div className="px-4 content">
+<p className="about-text" >The NFT Calendar team frequently reaches out to other institutions to work collaboratively in the NFT and crypto space.
+</p><p  className="about-text">If you are interested in partnering or working together, please <a href="https://forms.gle/jEC7c4Hgi5zxnoVn7" rel="nofollow" target="_blank">contact us!</a></p>
+</div>
+</div>
+</div>
+</section>
+        </>
+    )
+}
